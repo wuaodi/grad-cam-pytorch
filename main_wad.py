1e269bfc7dadcc59818572ed4fd63a0453b77126
wuaodi <54138120+wuaodi@users.noreply.github.com>
2021-05-05 22:24:52 +0800

    Update main_wad.py

--- main_wad.py
+++ main_wad.py
@@ -195,7 +195,7 @@ def demo4(image_paths, output_dir, cuda):
     model.eval()
 
     # The layers you want to see
-    target_layers = ["backbone.conv1","backbone.layer1.0.conv1","backbone.layer2.0.conv1","backbone.layer4.2.conv3"]
+    target_layers = ["layer1", "layer2", "layer3", "layer4"]
     target_class = 4    # "lake"
 
     # Images
